fd966528088a7da041abf99421c8d10d20105192
devrik3 <93089528+devrik3@users.noreply.github.com>
2021-12-06 12:33:20 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,34 +1,33 @@
 let form = document.getElementById('form'); //Chamando o </form>
-let nome = document.getElementById('POSTname'); //Chamando o nome
 let email = document.getElementById('POSTemail'); //Chamando o email
 let senha = document.getElementById('POSTpass');
 let erro = document.getElementById('erro'); //Chamando a duvida
 let cxErro = document.getElementById('boxError');
+
+let progress = document.getElementById('progress');
+
+let user = {
+              email:'dev@rik3.op',
+              password:'dev'
+            }
 form.addEventListener('submit', function(e){
-	if(nome.value == ''){
-		erro.innerHTML = 'Preencha todos os campos. :/';
-                var nameError = cxErro.style.display = 'block';
-                var timeOut = setTimeout(function timernone(){
-        	nameError = cxErro.style.display = 'none';
-        }, 2500);
-        e.preventDefault();
-	}else{}
+        if(email.value === user.email || senha.value === user.password){
+                e.preventDefault();
+                progress.style.display = 'flex';
 
-	if(email.value == ''){
-		erro.innerHTML = 'Preencha todos os campos. :/';
-                var nameError = cxErro.style.display = 'block';
-                var timeOut = setTimeout(function timernone(){
-        	nameError = cxErro.style.display = 'none';
-        }, 2500);
-        e.preventDefault();
-	}else{}
+                var timeOutRedirect = setTimeout(function timernone(){
+                        window.location.replace('index.html');
+                }, 1000);
+                e.preventDefault();
 
-        if(senha.value == ''){
+
+        }else{                
                 erro.innerHTML = 'Preencha todos os campos. :/';
                 var nameError = cxErro.style.display = 'block';
                 var timeOut = setTimeout(function timernone(){
                 nameError = cxErro.style.display = 'none';
-        }, 2500);
+                }, 2500);
+                e.preventDefault();
+        }
         e.preventDefault();
-        }else{}
 });

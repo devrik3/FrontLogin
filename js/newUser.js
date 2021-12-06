38839f5342f45f60d68bb0e8d890e96d597734a2
devrik3 <93089528+devrik3@users.noreply.github.com>
2021-12-06 12:45:12 -0300

    Update newUser.js

--- js/newUser.js
+++ js/newUser.js
@@ -4,10 +4,11 @@ let loginANIM = document.getElementById('loginBTN');
 let formANIM = document.getElementById('form');
 let registerCX = document.getElementById('formRegister');
 
+let footer = document.getElementById('footer');
 registerANIM.addEventListener('click', function(){
         formANIM.classList.toggle('animRegister'); //hide form
         registerCX.classList.toggle('animCX'); //view form
-
+        footer.setAttribute('style', 'display: none;')
         var timeOut = setTimeout(function timernone(){
             nameError = formANIM.style.display = 'none';
         }, 400);
@@ -16,7 +17,9 @@ registerANIM.addEventListener('click', function(){
 loginANIM.addEventListener('click', function(){
         formANIM.classList.toggle('animForm'); //view form
         registerCX.classList.toggle('animBX'); //hide form
-
+        setTimeout(() => {
+            footer.setAttribute('style', 'display: flex;')
+        }, 800);
 
         var timeOut = setTimeout(function timernone(){
             nameError = formANIM.style.display = 'flex';
@@ -26,6 +29,4 @@ loginANIM.addEventListener('click', function(){
             nameError = registerCX.style.display = 'none';
         }, 100);
 });
-
-
-
+// Create by @dev.rik3
